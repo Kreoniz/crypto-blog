58ab180a1842e919682db94598ee3c00e74e0077
Kreoniz <kreoniz@mail.ru>
2025-09-11 09:08:55 +0600

    build(deps): add and configure mathjax

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,19 @@
-// @ts-check
 import { defineConfig } from "astro/config";
 
 import vue from "@astrojs/vue";
 
 import mdx from "@astrojs/mdx";
+import rehypeMathjax from "rehype-mathjax";
+import remarkMath from "remark-math";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), mdx()],
+  integrations: [
+    vue(),
+    mdx({
+      syntaxHighlight: "prism",
+      remarkPlugins: [remarkMath],
+      rehypePlugins: [rehypeMathjax],
+    }),
+  ],
 });
-
